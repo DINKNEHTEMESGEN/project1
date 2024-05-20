a68ad0aa756bc0b02cb9bc17ad7aa5c52120bd1b
“DinknehTemesgen” <gjhomdinknehtemesgen@gmail.com>
2024-05-21 00:38:19 +0300

    this is task seven

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('test demo');
+function openham() {
+    const navigation1 = document.querySelector('.navigation');
+    navigation1.style.display = 'flex';
+}
+function closeham() {
+    const navigation1 = document.querySelector('.navigation');
+    navigation1.style.display = 'none';
+}
